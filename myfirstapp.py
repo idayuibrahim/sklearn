7a9170dcadbf13fbc7eb50946e52ece522784e29
idayuibrahim <87639502+idayuibrahim@users.noreply.github.com>
2021-07-24 02:03:27 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -48,7 +48,7 @@ if option=='Home' or option=="" :
 elif option=='Data Frame':
     st.write("This is the raw data with the selected features for the analysis")
     data = yf.download(StockCode, start=StartDate, end=EndDate)
-    data1 = data1 = data[['SPV','Close','CHG','Volume']]
+    data1 = data[['SPV','Close','CHG','Volume']]
     st.write('SPV (Spread Volatility) is differences between High and Low divided by Close price multiplied by 100') 
     st.write('CHG (Change) is percentage difference between Close and Open price') 
     st.write('Data Frame')
