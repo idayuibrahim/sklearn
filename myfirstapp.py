2f463cf8767d51ab6c9189c43d180b5f2b8a33a7
idayuibrahim <87639502+idayuibrahim@users.noreply.github.com>
2021-07-24 02:28:30 +0800

    Update myfirstapp.py

--- myfirstapp.py
+++ myfirstapp.py
@@ -53,6 +53,7 @@ elif option=='Data Frame':
     st.write('CHG (Change) is percentage difference between Close and Open price') 
     st.write('Data Frame')
     st.write(data1)
+    
     st.line_chart(data1.Close)
     st.line_chart(data1.Volume)
     st.line_chart(data1.SPV)
